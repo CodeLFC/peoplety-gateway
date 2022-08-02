07b79e2a378f0c30f4dd9af11d31301f623f72f1
LFC_dorm <1240902193@qq.com>
2022-08-02 14:07:23 +0800

    添加令牌桶流量控制规则

--- src/main/java/gaozhi/online/gateway/Application.java
+++ src/main/java/gaozhi/online/gateway/Application.java
@@ -9,8 +9,12 @@ package gaozhi.online.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 /**
  * 用户服务
@@ -22,5 +26,25 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
+
+    /**
+     * @description: 限流策略  获取访问者的ip地址, 通过访问者ip地址进行限流, 限流使用的是Redis中的令牌桶算法
+     * @param:
+     * @return: org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+     * @author LiFucheng
+     * @date: 2022/8/2 13:37
+     */
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                //获取访问者的ip地址, 通过访问者ip地址进行限流, 限流使用的是Redis中的令牌桶算法
+                String hostString = exchange.getRequest().getRemoteAddress().getHostString();
+                return Mono.just(hostString);
+            }
+        };
+    }
+
 }
 
